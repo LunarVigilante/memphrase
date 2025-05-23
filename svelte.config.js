df424d73dd2964f79461496381f29dc5e886a0ae
LunarVigilante <github@pirately.net>
2025-05-22 18:58:35 -0700

    feat: Configure SvelteKit adapter for Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({
+			// See https://kit.svelte.dev/docs/adapter-vercel for more information
+			// and https://vercel.com/docs/projects/project-configuration#project-settings/framework-detection
+			// for Vercel-specific options if needed.
+			runtime: 'nodejs18.x' // Or your desired Node.js runtime for Vercel serverless functions
+		})
+	},
 	extensions: ['.svelte', '.svx']
 };
 
